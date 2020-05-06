8867fa9eff126a4d4048516664b44b169cf3fd41
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-05-06 17:36:22 +0200

    Adds a global data file, used to store all the data from the databse.

--- AdministratorApp/Models/Data.cs
+++ AdministratorApp/Models/Data.cs
@@ -9,14 +9,35 @@ namespace AdministratorApp.Models
 {
     public static class Data
     {
-        public static List<Item> AllItems { get; set; }
-        public static List<Stock> AllStocks { get; set; }
+        public static Dictionary<int, Item> AllItems { get; set; } = new Dictionary<int, Item>();
+        public static Dictionary<int, Stock> AllStocks { get; set; } = new Dictionary<int, Stock>();
+        public static Dictionary<int, Store> AllStores{ get; set; } = new Dictionary<int, Store>();
+        public static Dictionary<int,Dictionary<int,int>> StockHasItems { get; set; } = new Dictionary<int, Dictionary<int, int>>();
+
+        
 
         public static async Task UpdateItems()
         {
-            AllItems = await APIHandler<Item>.GetMultiple("Items");
-            AllStocks = await APIHandler<Stock>.GetMultiple("Stocks");
+            AllItems = await APIHandler<Dictionary<int, Item>>.GetOne("Items");
+        }
+
+        public static async Task UpdateStock()
+        {
+            AllStocks = await APIHandler<Dictionary<int, Stock>>.GetOne("Stocks");
+        }
+
+        public static async Task UpdateStore()
+        {
+            AllStores = await APIHandler<Dictionary<int, Store>>.GetOne("Stores");
+        }
+
+        public static async Task UpdateStockHasItems()
+        {
+            StockHasItems = await APIHandler<Dictionary<int, Dictionary<int, int>>>.GetOne("StockHasItems");
         }
 
     }
+
+
+    
 }
