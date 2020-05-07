8ddcc3e530094c6f1498cddafd48c6a7a8ed9da0
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-05-07 15:37:19 +0200

    AddItem

---
+++ AdministratorApp/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdministratorApp.ViewModels
+{
+    class AddItemViewModel : INotifyPropertyChanged
+    {
+
+
+
+
+
+
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
