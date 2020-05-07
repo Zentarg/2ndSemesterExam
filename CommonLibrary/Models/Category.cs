2793abaa6293d6ecc7a866f1dcb3e9ffc039d611
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-05-07 11:51:08 +0200

    category

---
+++ CommonLibrary/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLibrary.Models
+{
+    class Category
+    {
+        public Category(int id, string name)
+        {
+            ID = id;
+            Name = name;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
