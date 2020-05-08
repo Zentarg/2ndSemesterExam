de0ed684419f4833759304291974e58f4234ba0d
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-05-08 11:57:54 +0200

    RelayCommand removed AddItemPage, StoreStockPage

--- AdministratorApp/ViewModels/AddItemViewModel.cs
+++ AdministratorApp/ViewModels/AddItemViewModel.cs
@@ -4,16 +4,34 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonLibrary.Models;
+using GalaSoft.MvvmLight.Command;
 
 namespace AdministratorApp.ViewModels
 {
     class AddItemViewModel : INotifyPropertyChanged
     {
+        public AddItemViewModel()
+        {
+            AddItemCommand = new RelayCommand(AddItem);
+        }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Dicount { get; set; }
+        public string Store { get; set; }
+        public int StoreID { get; set; }
+        public int Category { get; set; }
+        public int CategoryID { get; set; }
+        public string Color { get; set; }
+        public string Size { get; set; }
+        public string Comment { set; get; }
 
+        public RelayCommand AddItemCommand;
 
+        public void AddItem()
+        {
 
-
-
+        }
 
 
         event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
