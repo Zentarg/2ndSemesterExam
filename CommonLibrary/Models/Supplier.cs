54224bfa06b926a1b12236564472b6211fa31c59
Dominik Janos Hasko <domi1006@edu.easj.dk>
2020-05-18 12:52:26 +0200

    Supplier class

---
+++ CommonLibrary/Models/Supplier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLibrary.Models
+{
+    class Supplier
+    {
+        public Supplier(int id, string name, string address, string email, int phone)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Email = email;
+            Phone = phone;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
